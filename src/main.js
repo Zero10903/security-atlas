9969fe30dbe4f35e9bd43bb4e02b9ad742fedc4f
Zero <samuelbustos10903@gmail.com>
2023-03-19 20:23:58 -0300

    feat: scroll animations added

--- src/main.js
+++ src/main.js
@@ -10,6 +10,14 @@ const FAQ_ans_1 = document.querySelector('#FAQ__answer--n1');
 const FAQ_ans_2 = document.querySelector('#FAQ__answer--n2');
 const FAQ_ans_3 = document.querySelector('#FAQ__answer--n3');
 
+const card1 = document.getElementById('card--c1');
+const card2 = document.getElementById('card--c2');
+const card3 = document.getElementById('card--c3');
+const card4 = document.getElementById('card--c4');
+const FAQ1 = document.getElementById('FAQ--n1');
+const FAQ2 = document.getElementById('FAQ--n2');
+const FAQ3 = document.getElementById('FAQ--n3');
+
 buttons.forEach((button) => {
   button.addEventListener('click', (event) => {
     openBtn.classList.toggle('showBtn');
@@ -27,3 +35,27 @@ FAQ_btn_2.addEventListener('click', () => {
 FAQ_btn_3.addEventListener('click', () => {
   FAQ_ans_3.classList.toggle('FAQ__answer--show');
 });
+
+const cargarObjetos = (entradas) => {
+  entradas.forEach((entrada) => {
+    if (entrada.isIntersecting) {
+      entrada.target.classList.add('visible');
+    } else {
+      entrada.target.classList.remove('visible');
+    }
+  });
+};
+
+const observador = new IntersectionObserver(cargarObjetos, {
+  root: null,
+  rootMargin: '500px 0px 100px 0px',
+  threshold: 0.5,
+});
+
+observador.observe(card1);
+observador.observe(card2);
+observador.observe(card3);
+observador.observe(card4);
+observador.observe(FAQ1);
+observador.observe(FAQ2);
+observador.observe(FAQ3);
